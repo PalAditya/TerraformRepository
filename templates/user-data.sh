9de2570991026de48daff4349ffd73810fe64132
Aditya Pal <ap37@iitbbs.ac.in>
2021-11-28 14:10:17 +0530

    Manual install of Kafka and dockerized ZK works

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -4,11 +4,11 @@ set -x
 echo ECS_CLUSTER=${cluster_name} >> /etc/ecs/ecs.config
 mkdir -p /local
 mkdir -p /database
-#mkdir -p /vol1/zk-data
-#mkdir -p /vol2/zk-txn-logs
+mkdir -p /vol1/zk-data
+mkdir -p /vol2/zk-txn-logs
 #mkdir -p /vol3/kafka-data
-#chmod -R 777 /vol1/zk-data
-#chmod -R 777 /vol2/zk-txn-logs
+chmod -R 777 /vol1/zk-data
+chmod -R 777 /vol2/zk-txn-logs
 #chmod -R 777 /vol3/kafka-data
 
 sed -i '/After=cloud-final.service/d' /usr/lib/systemd/system/ecs.service
